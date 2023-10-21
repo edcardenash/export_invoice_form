ca1594bc22425c387779e996512a4c6e92ee1e47
Edgar Cárdenas <edcardenashorta@gmail.com>
2023-10-20 23:23:33 -0300

    asociations and validation sets

--- app/models/export_company.rb
+++ app/models/export_company.rb
@@ -1,2 +1,4 @@
 class ExportCompany < ApplicationRecord
+  has_many :invoices
+  validates :name, :rut, :address, presence: true
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,6 @@
 class Invoice < ApplicationRecord
   belongs_to :company
   belongs_to :export_company
+  has_many :containers
+  validates :invoice_number, :currency, :payment_terms, :bank_details, :icoterms, presence: true
 end
